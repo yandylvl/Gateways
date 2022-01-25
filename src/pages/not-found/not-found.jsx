a348e9e2866871d5f38cc421940a266d1af1a561
Yandy Viera <yvieram5@gmail.com>
2022-01-25 12:04:54 -0500

    Create not-found component

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -1,7 +1,19 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import "./not-found.scss";
 
 const NotFound = () => {
-  return <div>error 404</div>;
+  return (
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      className="gbContainer"
+      sx={{ height: { xs: "calc(100vh - 56px)", sm: "calc(100vh - 64px)" } }}
+    >
+      <Grid item xs className="bg-404"></Grid>
+    </Grid>
+  );
 };
 
 export default NotFound;
